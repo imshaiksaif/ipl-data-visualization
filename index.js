9a0be56a6a1e2558ceae1e87746fbd62d601010d
Saif Ali <saif.ali@mountblue.io>
2019-09-05 18:43:23 +0530

    written first function findTotalNumberOfMatchesPerYear of project

--- index.js
+++ index.js
@@ -13,4 +13,30 @@ csvtoJSON().fromFile("matches.csv").then((jsonArrayObj)=>
 const matchesJsonFile = require('./matchesJsonFile.json');
 // console.log(matchesJsonFile);
 
-hellomddfmfkdlf
+// first function : 1. Number of matches played per year for all the years in IPL.
+function findTotalNumberOfMatchesPerYear() {
+    let totalNumOfMatch = matchesJsonFile.reduce((accumulator, currentValue) => {
+        accumulator[currentValue.season] = (accumulator[currentValue.season] || 0) +1;
+        return accumulator;
+        
+    },{});
+    totalNumOfMatches = {'year': Object.keys(totalNumOfMatch), 'matches': Object.values(totalNumOfMatch)}; 
+    return totalNumOfMatches;
+}
+
+// console.log(findTotalNumberOfMatchesPerYear());
+// findTotalNumberOfMatchesPerYear();
+
+
+// // Object of year and matches
+
+findTotalNumberOfMatchesPerYear();
+
+let fullObject = {totalNumOfMatches};
+
+// ​
+// ​
+// // Dumping matchesPerYear in json file
+(async () => {
+    await writeToJSONFile('./public/data.json', fullObject);
+})();
