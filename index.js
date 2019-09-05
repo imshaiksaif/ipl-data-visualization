05b8ec8b70fb3c4442f57ed369366e52e142e35f
Saif Ali <saif.ali@mountblue.io>
2019-09-05 11:00:30 +0530

    Added csvtojson and write-json-file modules to convert csv files to json

---
+++ index.js
@@ -0,0 +1,14 @@
+//adding csvtojson module
+const csvtoJSON = require("csvtojson");
+
+//adding write-json-file module to write to json file
+const writeToJSONFile = require('write-json-file');
+
+csvtoJSON().fromFile("matches.csv").then((jsonArrayObj)=>
+{
+    (async () => {
+        await writeToJSONFile('matchesJsonFile.json', jsonArrayObj);
+    })();
+});
+const matchesJsonFile = require('./matchesJsonFile.json');
+// console.log(matchesJsonFile);
