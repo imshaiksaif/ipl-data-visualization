603ec3b91f208ab37104a3d27a1a474eee383733
Saif Ali <saif.ali@mountblue.io>
2019-09-07 11:29:12 +0530

    wrote third function to findtotal extra runs per team

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const csvtoJSON = require("csvtojson");
 
 //adding write-json-file module to write to json file
-const writeToJSONFile = require('write-json-file');
+const writeToJSONFile = require("write-json-file");
 
 csvtoJSON().fromFile("matches.csv").then((jsonArrayObj)=>
 {
@@ -11,9 +11,24 @@ csvtoJSON().fromFile("matches.csv").then((jsonArrayObj)=>
     })();
 });
 const matchesJsonFile = require('./matchesJsonFile.json');
+
 // console.log(matchesJsonFile);
 
-// first function : 1. Number of matches played per year for all the years in IPL.
+
+
+csvtoJSON().fromFile("deliveries.csv").then((jsonArrayObj)=>
+{
+    (async () => {
+        await writeToJSONFile("deliveries.json", jsonArrayObj);
+    })();
+    // console.log(jsonArrayObj);
+});
+const deliveriesArrObj = require("./deliveries.json");
+
+// console.log(deliveriesArrObj);
+
+
+// // first function : 1. Number of matches played per year for all the years in IPL.
 function findTotalNumberOfMatchesPerYear() {
     let totalNumOfMatch = matchesJsonFile.reduce((accumulator, currentValue) => {
         accumulator[currentValue.season] = (accumulator[currentValue.season] || 0) +1;
@@ -23,17 +38,6 @@ function findTotalNumberOfMatchesPerYear() {
     totalNumOfMatches = {'year': Object.keys(totalNumOfMatch), 'matches': Object.values(totalNumOfMatch)}; 
     return totalNumOfMatches;
 }
-
-// console.log(findTotalNumberOfMatchesPerYear());
-findTotalNumberOfMatchesPerYear();
-
-// // Object of year and matches
-let fullObject = {totalNumOfMatches};
-
-// writing output to json file in public folder
-(async () => {
-    await writeToJSONFile('./public/data.json', fullObject);
-})();
 
 
 
@@ -50,16 +54,59 @@ function matchesWonPerTeamPerYear() {
 
         } else {
             accumulator[currentValue.season] = {};
-
-            console.log(accumulator[currentValue.season][currentValue.winner] = 1);
-
+            accumulator[currentValue.season][currentValue.winner] = 1;
         }
 
         return accumulator;
     }, {});
 
-    console.log(matchesWon);
+    // console.log(matchesWon);
 
 }
 
 matchesWonPerTeamPerYear();
+
+
+// // third function : 3. Extra runs conceded per team in 2016
+
+function totalRunsConcededPerTeam() {
+    let collectionOfIds2016 = matchesJsonFile.filter(match => {
+        return match.season == '2016';
+    }).map(match => match.id);
+    // console.log(collectionOfIds2016);
+
+
+    let collectionOfMatchDelivery = deliveriesArrObj.filter(match => {
+        if(collectionOfIds2016.includes(match.match_id)){
+            return match;
+        }
+    });
+
+    // console.log(collectionOfMatchDelivery);
+
+    let extraRunsScoredPerTeam = collectionOfMatchDelivery.reduce((accumulator, currentValue) => {
+        accumulator[currentValue.bowling_team] = (accumulator[currentValue.bowling_team] || 0) + Number(currentValue.extra_runs);
+        return accumulator;
+    }, {})
+
+    let extraRunsFinalObj = {"teams": Object.keys(extraRunsScoredPerTeam), "runs": Object.values(extraRunsScoredPerTeam)};
+    // console.log(extraRunsFinalObj);
+    return extraRunsFinalObj; 
+}
+
+
+
+
+
+
+// console.log(findTotalNumberOfMatchesPerYear());
+let findTotalMatchesFunction = findTotalNumberOfMatchesPerYear();
+let totalRunsFunction = totalRunsConcededPerTeam();
+
+// // Object of year and matches
+let fullObject = {findTotalMatchesFunction, totalRunsFunction};
+
+// writing output to json file in public folder
+(async () => {
+    await writeToJSONFile("./public/data.json", fullObject);
+})();
